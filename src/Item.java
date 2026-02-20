790ad273f9cd4004ddb0e8884a2b3c796fd81c31
майор <ucebydla910@gmail.com>
2026-02-20 12:38:55 +0500

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Item {
+    private String name;
+    private double weight;
+
+    public Item(String name, double weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
 }
