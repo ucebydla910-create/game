c569a7635a17e0eab97367b384d3d88e6282a209
майор <ucebydla910@gmail.com>
2026-02-20 12:40:16 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        GameController game = new GameController();
+        GameUI ui = new GameUI(game);
+        ui.start();
+    }
 }
