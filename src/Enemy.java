b5bb9113faf196eb3f591e143197b5fafda3c5b3
майор <ucebydla910@gmail.com>
2026-02-20 12:37:05 +0500

    Update Enemy.java

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Enemy {
+    private String name;
+    private int health;
+    private int damage;
+    private EnemyState currentState;
+    // состояния...
+    public void attack(Player player) { currentState.attack(player); }
+    public void takeDamage(int dmg) { currentState.takeDamage(dmg); }
 }
